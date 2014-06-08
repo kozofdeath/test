4c83a2c9cf4342f63e9cbfdaf274622a67723ac9
Sam Kozloff <samkozloff89@gmail.com>
2014-06-07 22:58:13 -0500

    modifications to notes.ps1

--- notes.ps1
+++ notes.ps1
@@ -1,1 +1,16 @@
 #here is a txt file
+
+git status
+#tells you what has changed since your last commit
+
+How do you start tracking?
+git add FILE
+#now it's ready to be commited
+
+git commit -m "add this FILE"
+git status after this will...
+#show nothing really
+
+
+git workflow
+#Do a little bit of work, stage it (add) and make a commit (with a message)
